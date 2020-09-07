54caae96dca390f73a8a3248432b8d0b1bb66aca
anto-belo <70717945+anto-belo@users.noreply.github.com>
2020-09-07 23:26:16 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PL_Lab3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PL_Lab3.dir/main.c.obj"
+  "PL_Lab3.exe"
+  "PL_Lab3.exe.manifest"
+  "PL_Lab3.pdb"
+  "libPL_Lab3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PL_Lab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    printf("Input string:\n");
+    gets(s);
+
+    int length = strlen(s);
+    for (int i = 0; i < length; i++) {
+        if(length % 2 != 0 && i == length / 2) continue;
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
